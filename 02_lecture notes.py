bdb039a6c84a846fd3d22913d78ec0c34638a3b8
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-10-31 13:48:16 -0700

    Day 2 done

--- 02_lecture notes.py
+++ 02_lecture notes.py
@@ -2,143 +2,244 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
-
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
-
-
+​
+​
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
-
-
+​
+​
 class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
+​
     Implement this.
     """
-
+​
     def __init__(self, capacity):
-        # Your code here
-        if capacity >= MIN_CAPACITY:
-            self.capacity = max(capacity, MIN_CAPACITY)
-            self.mylist = [None] * self.capacity
-
+        
+        self.capacity = max(capacity, MIN_CAPACITY)
+        self.storage = [None] * self.capacity
+​
+        self.load = 0
+​
+​
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
+​
         One of the tests relies on this.
-
+​
         Implement this.
         """
-        # Your code here
-        return len(self.mylist)
-
+        return len(self.storage)
+​
+​
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
+​
         Implement this.
         """
         # Your code here
-        sum = 0
-        for x in self.mylist:
-            sum = sum + x
-        return sum % self.get_num_slots
-
+​
+# hashing functions used in:
+# git
+# cryptocurrencies
+# hash tables
+# store passwords
+​
+# choose between hashing functions
+## some are fast, some are slow
+​
+# "Can a hash be reversed?"
+# "How/Why not?"
+​
+## What's reversing a hash mean?
+## take a hash number and try to get back to the string it was made from
+​
+## p@$$w0rd
+## 0x23283287ad878f983efc
+​
+# deterministic
+# irreversible
+​
+# an attacker can't reverse, but could try hashing common passwords
+​
+## for a hash table, you want a fast function --> O(1)
+## for passwords, you want a slow function
+​
+​
+​
+​
+# Different strategies to handle collisions?
+## chaining: array of linked lists, with one LL per index, each node.next points to the second element
+## Array of arrays, with one array per index, just append
+## Disallow collisions?
+## Open addressing. Linear probing, quadratic probing. [None, 'hello', 'world', None]
+​
+​
+​
+​
+​
     def fnv1(self, key):
         """
-        FNV-1 Hash, 64-bit
-
-        Implement this, and/or DJB2.
+        set hash to 0?
+        maintain a total?
+​
+        - start hash at some large number(FNV_offset_basis)
+        - the hashed variable maintains our total
+​
+        some_big_prime * some_other_big_prime = some_mysterious_number
+​
+        
+        Comp Arch - bitwise operations, including XOR
+​
+        0101010101010
+     ^  1101101011001
+        -------------
+        1000111110011
+​
+​
         """
-
-        # Your code here
-        FNV_offset_basis = 14695981039346656037
+        FNV_offset_basis = 14695981039346656037 
         FNV_prime = 1099511628211
-
         hashed = FNV_offset_basis
-
+​
         bytes_to_hash = key.encode()
-
+​
         for byte in bytes_to_hash:
             hashed = hashed * FNV_prime
-            hashed = hashed ^ byte # 	XOR	Sets each bit to 1 if only one of two bits is 1
-            return hashed
-
+​
+            hashed = hashed ^ byte
+​
+        return hashed
+​
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
+​
         Implement this, and/or FNV-1.
+​
+        unsigned long
+        hash(unsigned char *str)
+        {
+            unsigned long hash = 5381;
+            int c;
+​
+            while (c = *str++)
+                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+​
+            return hash;
+        }
+        Left bitshifting
+        Left shift
+                   |
+   0101010101000000
+                   |
+       
+       Why 5381 and * 33? because they work!
+​
+       What's "work" - what makes these good?
+       - irreversible
+       - nice distribution, spreads them out over the array --> minimizes collisions
+        
         """
-        # Your code here
-        hash = 5381
-        for character in key:
-            hash = ((hash << 5)+hash)+ord(character)
-        return hash & 0xFFFFFFFF
-
+        hashed = 5381
+​
+        bytes_to_hash = key.encode()
+​
+        for byte in bytes_to_hash:
+            hashed = ((hashed << 5) + byte)
+            # hashed = ((hashed * 33) + byte)    # same
+​
+        return hashed
+​
+​
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-
+        #return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
-
+​
     def put(self, key, value):
         """
+        self.put(key, None)
+        -- will break our count!!
+​
         Store the value with the given key.
-
+​
         Hash collisions should be handled with Linked List Chaining.
-
+​
         Implement this.
         """
-        # Your code here
-        slot = self.hash_index(key)
-        self.mylist[slot] = value
-
+        # hash the key - self.hash_index will modulo it
+        idx = self.hash_index(key)
+​
+        # check for a collision
+        if self.storage[idx] != None:
+            print('warning! collision!!!')
+​
+        # insert the value at that location
+        self.storage[idx] = value
+​
+        self.load += 1
+​
+​
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
+​
         Print a warning if the key is not found.
-
+​
         Implement this.
+​
         """
-        # Your code here
-        self.put(key, None)
-
+        # hash the key to find index
+        idx = self.hash_index(key)
+​
+        if self.storage[idx] == None:
+            print('Warning! no key!!!')
+​
+        else:
+            self.storage[idx] = None
+​
+            self.load -= 1
+​
+​
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
+​
         Returns None if the key is not found.
-
+​
         Implement this.
         """
         # Your code here
-        return self.mylist[self.hash_index(key)]
-
+​
+​
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
+​
         Implement this.
         """
         # Your code here
-
-
+​
+​
+​
 if __name__ == "__main__":
     ht = HashTable(8)
-
+​
     ht.put("line_1", "'Twas brillig, and the slithy toves")
     ht.put("line_2", "Did gyre and gimble in the wabe:")
     ht.put("line_3", "All mimsy were the borogoves,")
@@ -151,22 +252,26 @@ if __name__ == "__main__":
     ht.put("line_10", "Long time the manxome foe he sought--")
     ht.put("line_11", "So rested he by the Tumtum tree")
     ht.put("line_12", "And stood awhile in thought.")
-
+​
     print("")
-
+​
     # Test storing beyond capacity
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
-
+​
     # Test resizing
     old_capacity = ht.get_num_slots()
     ht.resize(ht.capacity * 2)
     new_capacity = ht.get_num_slots()
-
+​
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-
+​
     # Test if data intact after resizing
     for i in range(1, 13):
         print(ht.get(f"line_{i}"))
-
+​
     print("")
+Collapse
+
+
+
--- hashtable/test_hashtable.py
+++ hashtable/test_hashtable.py
@@ -1,6 +1,6 @@
 import unittest
 
-from hashtable import HashTable
+from hashtable3 import HashTable
 
 
 class TestHashTable(unittest.TestCase):
