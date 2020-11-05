18e8e4a109f9a4d5091d00f56219db3b9dc7d01f
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-11-04 18:43:44 -0800

    day 3 lecture notes

--- 04_lecture_notes.py
+++ 04_lecture_notes.py
@@ -22,3 +22,60 @@ def how_many_before_collision(num_buckets):
             tries +=1 
     return tries
 print(how_many_before_collision(10330))
+
+
+
+
+# memoization, closely related to dynamic programming
+## DP: top down, break the problem up as you
+## reuse previous results
+## key is what you have, value is what you calculate 
+
+# fibonacci sequence
+## a function that returns the n-th item in the fibonacci sequence
+## golden proportion
+
+## 0 1 1 2 3 5 8 13 21 34 55 89
+### Kanban board: card holds a feature, "make this button"
+### 1 2 3 5 8 13 21 
+
+# let's do it recursively
+
+# need base case
+# progress toward base case
+
+cache = {}
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = fib(n - 1) + fib(n - 2)
+        return cache[n]
+
+print(fib(3)) # should be 2
+print(fib(6)) # should be 8
+print(fib(11)) # should be 89
+
+
+# expensive calculation on the fly
+import math
+
+lookup_table = {}
+
+def inverse_root(n):
+    return 1/math.sqrt(n)
+
+for i in range(1, 1000):
+    lookup_table[i] = inverse_root(i)
+
+print(lookup_table[995])
+
+# rainbow table
+## hash common passwords ahead of time
+## precomputed table for caching the output of cryptographic hash functions,
+# usually for cracking password hashes
+
+# hashing function for pws should be slow
